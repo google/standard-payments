3b3e94451aba224bf790b5f30fc94d89b2409fea
Pedro Reta <pedroreta@google.com>
2020-06-19 20:26:51 +0100

    First sample
    Includes the initial structire of the repository and the first sample.

---
+++ samples/pgp/src/main/java/com/google/samples/pgp/KeyManager.java
@@ -0,0 +1,34 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.samples.pgp;
+
+import java.io.InputStream;
+
+/**
+ * A key storage.
+ */
+public interface KeyManager {
+    void addPublicKeys(InputStream inputStream) throws KeyManagementException;
+    void addSecretKeys(InputStream inputStream, char[]... passphrases)
+        throws KeyManagementException;
+
+    /**
+     * Custom exception for any key-related error.
+     */
+    class KeyManagementException extends Exception {
+        KeyManagementException(Throwable throwable) {
+            super(throwable);
+        }
+    }
+}
